b1a2485a1c5152487a47b9a2353a601655db1a2e
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2023-01-04 00:29:06 -0500

    Update mod.rs
    - Fixed fall method
    - Implemented cursor rotation
    - Implemented cursor movement
    - Implemented cursor scaling
    - Added cursor info to debug

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,4 +1,4 @@
-use std::{array, fmt::{Debug, Display}};
+use std::{array, fmt::{Debug, Display}, cmp::{min, max}, borrow::BorrowMut};
 
 use self::{row::Row, cell::Cell, cursor::Cursor};
 
@@ -7,6 +7,15 @@ pub mod row;
 mod cursor;
 
 pub type Board<const WIDTH: usize, const HEIGHT: usize> = [Row<WIDTH>; HEIGHT];
+
+pub enum CursorPosChangeDir{
+    Grow,
+    Shrink
+}
+pub enum CursorRotChangeDir{
+    Clockwise,
+    CounterClockwise
+}
 
 pub struct Game<const WIDTH: usize, const HEIGHT: usize> {
     pub board: Board<WIDTH, HEIGHT>,
@@ -26,11 +35,20 @@ impl<const WIDTH: usize, const HEIGHT: usize> Game<WIDTH, HEIGHT> {
     }
 
     pub fn fall(&mut self) {
-        'column_loop: for x in 0..WIDTH{
-            if let (cell::Cell::Filled(_), cell::Cell::Filled(_)) = (&self.board[0][x], &self.board[1][x]) {
-                continue 'column_loop;
-            }
-            'row_loop: for y in 2..HEIGHT{
+        let mut start = HEIGHT;
+        '_column_loop: for x in 0..WIDTH{
+            start = (1..HEIGHT).
+                find(
+                    |y| {
+                        if let (cell::Cell::Filled(_), cell::Cell::Empty) = (&self.board[*y - 1][x], &self.board[*y][x]) {
+                            return true;
+                        }
+                        return false;
+                    }
+                )
+                .unwrap_or(HEIGHT);
+            
+            'row_loop: for y in start..HEIGHT{
                 match (&self.board[y - 1][x], &self.board[y][x]) {
                     //next row
                     (cell::Cell::Filled(_), cell::Cell::Empty) |
@@ -49,7 +67,90 @@ impl<const WIDTH: usize, const HEIGHT: usize> Game<WIDTH, HEIGHT> {
             }
         }
 
-        (self.new_row)(&mut self.board[HEIGHT-1]);
+        if start != HEIGHT {
+            (self.new_row)(&mut self.board[HEIGHT-1]);
+        }
+    }
+
+    pub fn change_cursor_length(&mut self, dir: CursorPosChangeDir) {
+        match dir{
+            CursorPosChangeDir::Grow => {
+                let mut new_cursor = self.cursor;
+                new_cursor.double();
+
+                if HEIGHT < new_cursor.length || WIDTH < new_cursor.length {
+                    return ;
+                }
+
+                Self::fix_cursor_pos(&mut new_cursor);
+
+                self.cursor = new_cursor;
+            },
+            CursorPosChangeDir::Shrink => {
+                self.cursor.shrink();
+            },
+        }
+    }
+
+    pub fn move_cursor(&mut self, delta_x: i8, delta_y: i8) {
+
+        let mut new_cursor = self.cursor;
+
+        new_cursor.pivot.0 = self.cursor.pivot.0 + delta_x;
+        new_cursor.pivot.1 = self.cursor.pivot.1 + delta_y;
+
+        Self::fix_cursor_pos(&mut new_cursor);
+
+        self.cursor = new_cursor;
+    }
+
+    fn fix_cursor_pos(cursor: &mut Cursor) {
+        Self::fix_cursor_x_axis(cursor);
+        
+        Self::fix_cursor_y_axis(cursor);
+    }
+
+    fn fix_cursor_x_axis(cursor: &mut Cursor) {
+        let mut end = cursor.end();
+
+        let min = min(cursor.pivot.0, end.0);
+        let max = max(cursor.pivot.0, end.0);
+
+        if min < 0 {
+            cursor.pivot.0 += 0 - min;
+            end = cursor.end();
+        }
+        else if WIDTH <= max as usize {
+            cursor.pivot.0 += (WIDTH - 1) as i8 - max;
+            end = cursor.end();
+        }
+    }
+
+    fn fix_cursor_y_axis(cursor: &mut Cursor) {
+        let mut end = cursor.end();
+        
+        let min = min(cursor.pivot.1, end.1);
+        let max = max(cursor.pivot.1, end.1);
+
+        if min < 0 {
+            cursor.pivot.1 += 0 - min;
+        }
+        else if HEIGHT <= max as usize {
+            cursor.pivot.1 += (HEIGHT - 1) as i8 - max;
+        }
+    }
+
+    pub fn change_cursor_rot(&mut self, rot: CursorRotChangeDir){
+        let mut new_cursor = self.cursor;
+
+        match rot {
+            CursorRotChangeDir::Clockwise => new_cursor.clockwise_rot(),
+            CursorRotChangeDir::CounterClockwise => new_cursor.counter_clockwise_rot(),
+        }
+
+        Self::fix_cursor_pos(&mut new_cursor);
+
+        self.cursor = new_cursor;
     }
 }
 
@@ -59,7 +160,7 @@ impl<const WIDTH: usize, const HEIGHT: usize> Default for Game<WIDTH, HEIGHT> {
         
         Game{
             board: board,
-            new_row: Row::empty,
+            new_row: Row::random,
             cursor: Cursor::default(),
         }
     }
@@ -67,14 +168,52 @@ impl<const WIDTH: usize, const HEIGHT: usize> Default for Game<WIDTH, HEIGHT> {
 
 impl<const WIDTH: usize, const HEIGHT: usize> Debug for Game<WIDTH, HEIGHT> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let cursor_range = {
+
+            let start = self.cursor.pivot;
+            let end = self.cursor.end();
+
+            (
+                (min(start.0, end.0), min(start.1, end.1)),
+                (max(start.0, end.0), max(start.1, end.1))
+            )
+        };
+
         let board = self.board
             .iter()
+            .map(|row| format!("{:?}", row))
+            .enumerate()
             .rev()
+            .map(
+                |(y, row)|{
+                    let mut row = row; 
+
+                    if cursor_range.0.1 as usize <= y &&  y <= cursor_range.1.1 as usize {
+
+                        let mut tmp = String::from("");
+                    
+                        let mut chars = row.chars();
+                        for i in 0..(cursor_range.0.0 as usize) {
+                            tmp.push(chars.next().unwrap());
+                        }
+
+                        for x in (cursor_range.0.0 as usize)..=(cursor_range.1.0 as usize) {
+                            tmp.push('X');
+                            chars.next();
+                        }
+
+                        chars.for_each(|c| tmp.push(c));
+
+                        row = tmp;
+                    }
+                    (y, row)
+                }
+            )
             .fold(
-                String::from("Game"),
-                |acc, row| format!("{}\n{:?}", acc, row)
+                format!("DEBUG{}GAME", " ".repeat(WIDTH-"DEBUG".len()+4)),
+                |acc, row| format!("{}\n{}", acc, row.1)
             );
-        write!(f, "{}", board)
+        write!(f, "{}\nCursor:{:?}", board, self.cursor)
     }
 }
 
